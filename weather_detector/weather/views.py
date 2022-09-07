40ebc31c2b5e754bad0e3aa6634168f0638ecd5a
ruslan-kornich <korny4rus143@gmail.com>
2022-09-07 21:46:37 +0300

    init

---
+++ weather_detector/weather/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+    else:
+        city = ''
+    return render(request, 'index.html', {'city': city})
