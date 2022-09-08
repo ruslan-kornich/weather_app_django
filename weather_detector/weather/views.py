68192ba2298371e99ea59b661ddb490cb0d1c0b5
ruslan-kornich <korny4rus143@gmail.com>
2022-09-08 21:21:27 +0300

    fix some bugs

--- weather_detector/weather/views.py
+++ weather_detector/weather/views.py
@@ -1,8 +1,24 @@
+import json
+import urllib.request
+
 from django.shortcuts import render
+
 
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
+        res = urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=bb5ff8282972f79e7666a4968381db98').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp']) + 'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+
     else:
         city = ''
-    return render(request, 'index.html', {'city': city})
+        data = {}
+    return render(request, 'index.html',  {'city': city, 'data': data})
